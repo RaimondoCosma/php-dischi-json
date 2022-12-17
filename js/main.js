acfaa2c2650cae89fc4a5f0c7643be0ba9fb3c85
RaimondoCosma <raimondo.cosma@gmail.com>
2022-12-17 18:09:31 +0100

    Aggiunge dettagli albulm al click

--- js/main.js
+++ js/main.js
@@ -4,7 +4,18 @@ createApp({
   data() {
     return {
       disks: [],
+      diskClicked: "",
+      active: false,
     };
+  },
+  methods: {
+    getIndex(index) {
+      this.toggleOffcanvas();
+      this.diskClicked = index;
+    },
+    toggleOffcanvas() {
+      this.active = !this.active;
+    },
   },
   created() {
     axios.get("http://localhost/php-dischi-json/php/api.php").then((resp) => {
